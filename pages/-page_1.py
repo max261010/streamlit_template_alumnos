537f7f7e79238f7475ce48bb34caa9a26aa75d66
max261010 <127776880+max261010@users.noreply.github.com>
2023-03-14 14:46:55 +0100

    Update -page_1.py

--- pages/-page_1.py
+++ pages/-page_1.py
@@ -1,1 +1,14 @@
+#--------------------LIBRERÍAS----------------------------#
+import streamlit as st 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import plotly_express as px
+import plotly.graph_objects as go
 
+
+#--------------------CONFIGURACIÓN DE LA PÁGINA----------------------------#
+#layout="centered" or "wide"
+st.set_page_config(page_title="Mi primera APP", layout="wide", page_icon="👋")
+st.set_option('deprecation.showPyplotGlobalUse', False)
